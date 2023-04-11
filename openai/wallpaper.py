d081cf8fe15283724a33223d94ace0c687812172
João Victor <pipoca.play19@gmail.com>
2023-04-10 23:42:53 -0300

    update

---
+++ openai/wallpaper.py
@@ -0,0 +1,28 @@
+import openai as oi
+
+
+oi.api_key = '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'
+
+
+response = oi.Image.create(
+    prompt='painting by da vinci',
+
+    n=1,
+    size='1024x1024'
+)
+
+img_url = response['data'][0]['url']
+print(f'img_url: {img_url}')
+
+
+'''except: 
+    response = oi.Image.create(
+        prompt='I love u',
+        n=1,
+        size='1024x1024'
+    )
+    
+    img_url = response['data'][0]['url']
+    print(f'img_url: {img_url}')
+
+'''
